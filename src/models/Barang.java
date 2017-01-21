59fb9e138759027fada2bbae9bbbc4ef5e218d42
maylantinosurya <maylantino.surya@gmail.com>
2017-01-21 12:16:42 +0700

    Model Barang

--- src/models/Barang.java
+++ src/models/Barang.java
@@ -11,4 +11,50 @@ package models;
  */
 public class Barang {
     
+    int harga;
+    int quantity;
+    String nama;
+    String ID;
+
+    public Barang(int harga, int quantity, String nama, String ID) {
+        this.harga = harga;
+        this.quantity = quantity;
+        this.nama = nama;
+        this.ID = ID;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
+    
+    
 }
