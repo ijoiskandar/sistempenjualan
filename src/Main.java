ede9b3dc57a3dcb745f89cbc27ed0a5450f24ba8
Alfian Hidayat <alfian08.ah@gmail.com>
2017-01-21 12:34:10 +0700

    tampil barang

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+
+import controllers.AmbilBarang;
+import java.util.ArrayList;
+import models.Barang;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +16,11 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        AmbilBarang ab = new AmbilBarang();
+        ArrayList<Barang> data = ab.getBarangs();
+        for (Barang string : data) {
+            System.out.println(string.getNama());
+            System.out.println(string.getHarga());
+        }
     }
 }
---
+++ src/controllers/AmbilBarang.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import models.Barang;
+
+/**
+ *
+ * @author alfianh
+ */
+public class AmbilBarang {
+    
+    
+    public ArrayList<Barang> getBarangs(){
+        String fileName = "stock.txt";
+        File f = new File(fileName);
+        ArrayList<Barang> list = new ArrayList<>();
+        try{
+            BufferedReader baca = new BufferedReader(new FileReader(f));
+            String line ="";
+            while((line=baca.readLine())!=null){
+                String arr[] = line.split(";");
+                Barang b = new Barang(Integer.parseInt(arr[0]),Integer.parseInt(arr[1]), arr[2], arr[3]);
+                list.add(b);
+            }
+            baca.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+    
+}
