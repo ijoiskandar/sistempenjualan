1213c9b8bad5c8b4a261bc694b22bf74a4049706
maylantinosurya <maylantino.surya@gmail.com>
2017-01-21 12:43:29 +0700

    Add barang 100 persen

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,22 @@
  * and open the template in the editor.
  */
 
+import controllers.AddBarang;
 import java.util.Scanner;
+
 /**
  *
  * @author alfianh
  */
 public class Main {
+
     public static void main(String[] args) {
+        Main mm = new Main();
+        mm.menu();
+    }
+
+    public void menu() {
+
         System.out.println("1. View Item.");
         System.out.println("2. Add Item.");
         System.out.println("3. Delete Item.");
@@ -19,21 +28,26 @@ public class Main {
         System.out.println("0. Quit.");
         Scanner input = new Scanner(System.in);
         String pilihMenu = input.nextLine();
-        do{
-            switch(pilihMenu){
-                case "1":
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                default:
-                    break;
-            }
-        }while(pilihMenu !="0");
+        
+        switch (pilihMenu) {
+            case "1":
+                break;
+            case "2":
+                
+                AddBarang add = new AddBarang();
+                add.TambahBarang("4", "Tango", 10, 400);
+
+                menu();
+                
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            default:
+                break;
+        }
     }
 }
